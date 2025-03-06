c59adb68dd6f6c70727d0a725f2053cdb0255e09
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2025-03-06 22:09:01 +0530

    fix: add hosted BE URL as default🚑️

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,9 @@ export const env = createEnv({
   client: {
     VITE_NODE_ENV: z.string().default("development"),
     VITE_PORT: z.coerce.number().default(4173),
-    VITE_API_URL: z.string().default("http://localhost:8999/api/v1"),
+    VITE_API_URL: z
+      .string()
+      .default("https://finance-management-api-c8hw.onrender.com/api/v1"),
   },
   runtimeEnv: {
     NODE_ENV: import.meta.env.VITE_NODE_ENV,
