e0f3b534fb8a19623b79d9a8570d05869524dff9
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2025-03-22 14:29:24 +0530

    refactor: IS_PRODUCTION env name♻️

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,21 +2,18 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
-import { IS_ENV_PRODUCTION } from "@/utils/constants";
+import { IS_PRODUCTION } from "@/utils/constants";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      {
-        !IS_ENV_PRODUCTION
-        && (
-          <>
-            <ReactQueryDevtools buttonPosition="bottom-right" />
-            <TanStackRouterDevtools />
-          </>
-        )
-      }
+      {!IS_PRODUCTION && (
+        <>
+          <ReactQueryDevtools buttonPosition="bottom-right" />
+          <TanStackRouterDevtools />
+        </>
+      )}
     </>
   ),
 });
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,8 @@
-export const API_URL
-  = import.meta.env.VITE_NODE_ENV === "production"
-    ? import.meta.env.VITE_API_URL
-    : import.meta.env.VITE_API_URL_LOCAL;
+export const IS_PRODUCTION = import.meta.env.VITE_NODE_ENV === "production";
+export const API_URL = IS_PRODUCTION
+  ? import.meta.env.VITE_API_URL
+  : import.meta.env.VITE_API_URL_LOCAL;
 
 export const LOGIN_URL = `${API_URL}/auth/login`;
 export const REGISTER_URL = `${API_URL}/auth/register`;
 export const LOGOUT_URL = `${API_URL}/auth/logout`;
-
-export const IS_ENV_PRODUCTION = import.meta.env.VITE_NODE_ENV === "production";
