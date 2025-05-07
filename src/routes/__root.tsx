3d149cbf91e2ff34d5fdcea9cf9e68f68715d9b2
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2025-05-07 23:47:50 +0530

    fix: add gtag, ms clarity scripts🐛

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -56,6 +56,39 @@ function RootDocument({ children }: { children: React.ReactNode }) {
     <html>
       <head>
         <HeadContent />
+
+        {/* Google Tag Manager snippet */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() {
+                dataLayer.push(arguments);
+              }
+              gtag("js", new Date());
+
+              gtag("config", "G-TRCB701S2S");
+            `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function (c, l, a, r, i, t, y) {
+                c[a] =
+                  c[a] ||
+                  function () {
+                    (c[a].q = c[a].q || []).push(arguments);
+                  };
+                t = l.createElement(r);
+                t.async = 1;
+                t.src = "https://www.clarity.ms/tag/" + i;
+                y = l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t, y);
+              })(window, document, "clarity", "script", "qm5cfatim4");
+            `,
+          }}
+        />
       </head>
       <body>
         <QueryClientProvider client={queryClient}>
